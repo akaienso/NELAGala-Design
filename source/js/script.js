1d4d148ad6c939fc5ec59fd615074d243f06d27a
julia <yulia.babiy.1994@gmail.com>
2024-03-12 16:55:34 +0200

    navbar

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburger = document.querySelector('.hamburger');
-    var navList = document.querySelector('.event-navigation ul');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var hamburger = document.querySelector('.hamburger');
+//     var navList = document.querySelector('.event-navigation ul');
 
-    hamburger.addEventListener('click', function() {
-        hamburger.classList.toggle('open');
-        navList.classList.toggle('open');
-    });
-});
+//     hamburger.addEventListener('click', function() {
+//         hamburger.classList.toggle('open');
+//         navList.classList.toggle('open');
+//     });
+// });
+
+
+(function(){
+    var burger = document.querySelector('.burger-container'),
+        nav = document.querySelector('.nav');
+    
+    burger.onclick = function() {
+        nav.classList.toggle('menu-opened');
+    }
+}());
 
 //  Google Maps API Key: AIzaSyBGZLfom_9gzVfPI39FCQ1MHWGxNjxUqDg
